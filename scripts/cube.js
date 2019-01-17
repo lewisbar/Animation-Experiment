78790f5739086dd3c61226725348cb3d6c1a4d52
Lennart Wisbar <lewisbar.git@gmail.com>
2019-01-17 11:47:30 +0100

    Added a comment

--- scripts/cube.js
+++ scripts/cube.js
@@ -2,6 +2,9 @@ var scene = document.getElementById("scene1");
 
 function wheelAction(event) {
     let oldPerspective = parseInt(getComputedStyle(scene).perspective);
+    // If we view x as the scroll level and y as the perspective,
+    // we need to first get the x for the current perspective to 
+    // add it to the value of the new scroll action in the equasion
     let xForPer = (oldPerspective - 150) ** (1 / 1.9);
     let newPerspective = (((xForPer + event.deltaY) ** 1.9) + 150);
     if (newPerspective > 1200) {
