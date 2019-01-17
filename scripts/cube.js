e8824f2358ef594dd0e83ef5dbf239e7fc036d2c
Lennart Wisbar <lewisbar.git@gmail.com>
2019-01-17 10:53:23 +0100

    Added a folder structure

--- scripts/cube.js
+++ scripts/cube.js
@@ -41,7 +41,14 @@ var scene = document.getElementById("scene1");
 // scene.addEventListener("click", changePerspective);
 
 function wheelAction(event) {
-    var per = parseInt(getComputedStyle(scene).perspective);
-    scene.style.perspective = per + event.deltaY * 120;
+    let oldPerspective = parseInt(getComputedStyle(scene).perspective);
+    // let shallowDelta = 3 * Math.atan(event.deltaY);
+    let xForPer = (oldPerspective - 150) ** (1 / 1.9);
+    let newPerspective = (((xForPer + event.deltaY) ** 1.9) + 150);
+    if (newPerspective > 1200) {
+        newPerspective = 1200;
+    }
+
+    scene.style.perspective = newPerspective;
 }
 scene.addEventListener("wheel", wheelAction);
